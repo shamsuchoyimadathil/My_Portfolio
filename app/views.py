605ce8fd729c1d9522e529b3148e14cf058e767c
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-09-07 13:56:19 +0530

    added styles to main page

--- app/views.py
+++ app/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
 
-# class Main_page(TemplateView):
-
-#     template_name = "main.html"
 
 def main(request):
 
