ebf602262caea125b6599f1a4530a4da7e7bc814
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-09-29 20:55:37 +0530

    almost finished

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static 
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path("",views.main,name="main"),
-    # path("",views.email,name="email"),
 
 ]
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from abc import ABCMeta
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 import os 
 
 
 content = (
-    {"des":"it's simple recipe app , you can add recipes , search recipes ,etc ...",
+    {"des":"It's a simple recipe app. You can add recipes, search recipes, etc.",
     "name":"recipe app","img":"main_background.jpeg",},
 
     {"des":"it's simple photo app ,you can download images , also upload to this app",
@@ -27,26 +30,26 @@ content = (
 
 
 def main(request):
-
     context = {}
 
     if request.method == "POST":
         name = request.POST['name']
         email = request.POST['email']
-        message = request.POST['message']
+        message = request.POST['message'] 
 
-        send_mail(
-            "message from"+email ,
-            "hey i am"+name,
+        if name == "" or email == "" or message == "":
+            context["no_info"] = "please complete the fields"
+            return render(request,"main.html",context)
+        else:
+            send_mail(
+            "message from "+email ,
             message,
+            "hey i am "+name,
             ["shamsudheenchoyimadathil@gmail.com"],
             fail_silently=False,
-        )
+            )
+            return HttpResponseRedirect(reverse('main'))
 
-        return render(request,"main.html") 
-    
-        
     context["details_of_works"] = content
 
     return render(request,"main.html",context)
-    
