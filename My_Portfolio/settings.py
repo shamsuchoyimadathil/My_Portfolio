8721d8509824e132a51f149a841d07c4ca3dd43d
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-09-30 23:30:08 +0530

    modified sttings again

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, 'static/'),
-]
+# STATICFILES_DIRS = [ 
+#     os.path.join(BASE_DIR, 'static/'),
+# ]
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
@@ -132,14 +132,12 @@ STATICFILES_DIRS = [
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'static/static_files')
 
-
-
-# if DEBUG:
-#    STATICFILES_DIRS = [ 
-#     os.path.join(BASE_DIR, 'static/static_files'),
-# ]
-# else:
-#    STATIC_ROOT = os.path.join(BASE_DIR,'static/static_files')
+if DEBUG:
+   STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static/static_files'),
+]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static/static_files')
 
 
 
