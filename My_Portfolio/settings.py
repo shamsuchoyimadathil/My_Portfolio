ed668691867ab2063d6603dbb47ede0586797ac3
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2021-09-30 10:59:46 +0530

    modified settings for static

--- My_Portfolio/settings.py
+++ My_Portfolio/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'static/static_files'),
 ]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
